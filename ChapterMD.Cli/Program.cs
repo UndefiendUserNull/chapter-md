2ca0d19970fc95e6c2d6484d6d70d10e09f5c674
UndefiendUserNull <viski8548@gmail.com>
2026-09-17 17:38:32 +0300

    Fixed project structure

---
+++ ChapterMD.Cli/Program.cs
@@ -0,0 +1,44 @@
+﻿using CommandLine;
+
+namespace ChapterMD.Cli;
+
+public static class Program
+{
+    class Options
+    {
+        [Option('m', "name", Required = true, Default = ".", HelpText = "File name.")]
+        public string FileName { get; set; } = string.Empty;
+
+        [Option(
+          Default = false,
+          HelpText = "Prints all messages to standard output.")]
+        public bool Verbose { get; set; }
+
+        [Value(5, MetaName = "amount", HelpText = "The amount of chapters inside the file.")]
+        public int ChaptersAmount { get; set; }
+    }
+
+    static int Main(string[] args)
+    {
+        CommandLine.Parser.Default.ParseArguments<Options>(args)
+          .WithParsed(RunOptions)
+          .WithNotParsed(HandleParseError);
+
+        return 0;
+    }
+    static void RunOptions(Options opts)
+    {
+        if (opts.FileName != string.Empty && opts.ChaptersAmount > 0)
+        {
+            Core.ChapterFileWriter.WriteChapterFile(opts.FileName, opts.ChaptersAmount);
+        }
+    }
+    static void HandleParseError(IEnumerable<Error> errs)
+    {
+        foreach (var item in errs)
+        {
+            throw new Exception(item.ToString());
+        }
+    }
+
+}
---
+++ ChapterMD.Core/Core.cs
@@ -0,0 +1,13 @@
+﻿namespace ChapterMD.Core;
+
+public static class ChapterFileWriter
+{
+    public static void WriteChapterFile(string path, int chaptersAmount)
+    {
+        using var writer = new StreamWriter(path, append: false);
+        for (int i = 1; i <= chaptersAmount; i++)
+        {
+            writer.WriteLine($"- [ ] Chapter {i}");
+        }
+    }
+}
